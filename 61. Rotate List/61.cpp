b7fbec880a364f31430dc2c041489451c5e9d513
sdt03 <dateraoshoumik@gmail.com>
2025-02-16 17:18:43 +0530

    Init

---
+++ 61. Rotate List/61.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode* rotateRight(ListNode* head, int k) {
+            if(head==NULL || head->next==NULL || k==0) return head;
+    
+            ListNode* temp = head;
+            int count = 1;
+            while(temp->next!=NULL){
+                ++count;
+                temp=temp->next;
+            } 
+    
+            temp->next=head;   
+            k=k%count;
+            int end = count - k;
+            while(end--) temp=temp->next;
+            head = temp->next;
+            temp->next = NULL;
+            return head;
+        }
+    };
---
+++ 61. Rotate List/README.md
@@ -0,0 +1,27 @@
+### <a href="https://leetcode.com/problems/rotate-list/description/"> 61. Rotate List </a>
+
+<img src='https://img.shields.io/badge/Difficulty-Medium-yellow' alt='Difficulty: Medium' />
+
+---
+<br />
+
+Given the ```head``` of a linked list, rotate the list to the right by ```k``` places. <br />
+
+### Example 1:
+<img src = "https://assets.leetcode.com/uploads/2020/11/13/rotate1.jpg">
+
+Input: ```head = [1,2,3,4,5], k = 2``` <br />
+Output: ```[4,5,1,2,3]``` <br />
+
+### Example 2:
+<img src="https://assets.leetcode.com/uploads/2020/11/13/roate2.jpg">
+
+Input: ```head = [0,1,2], k = 4``` <br />
+Output: ```[2,0,1]``` <br />
+ 
+
+### Constraints:
+
+- The number of nodes in the list is in the range ```[0, 500]```.
+- ```-100 <= Node.val <= 100```
+- ```0 <= k <= 2 * 109```
