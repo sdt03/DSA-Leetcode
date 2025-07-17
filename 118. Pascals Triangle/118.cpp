8539f323515112b9997ef17ffb042fb53f039f00
sdt03 <dateraoshoumik@gmail.com>
2025-07-17 18:45:11 +0530

    Q118

---
+++ 118. Pascals Triangle/118.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int ncr(int n, int r){
+        long long res = 1;
+        for(int i=0;i<r;i++){
+            res = res * (n-i);
+            res = res / (i+1);
+        }
+        return (int)(res);
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+
+        for(int row = 1; row<=numRows;row++){
+            vector<int> temp;
+            for(int col=1;col<=row;col++){
+                temp.push_back(ncr(row-1, col-1));
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
